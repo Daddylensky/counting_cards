0cc4a43443a33d70c583a80906cc2e0bd4a0332f
Daddylensky <obo.dan87@gmail.com>
2023-05-07 17:06:49 -0400

    added proper position of all cards, added functionality of asking for my cards

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# counting_cards
+This website (hopefully functioning) will teach you how to count cards
+
+
+
+shit to implement 
+#1 make the website more visually appealing
+    a. an actual blackjack background
+    b. implement counting cards logic
+    c. appropriate text all around the game and cards img that are high quality 
+
+#2 a leaderboard??
+
+#3 game functionality 
+    a. when you press the "deal cards" button twice, it puts two more to the right, instead of on top. //FIX//
+    b. ¯\_(ツ)_/¯
+
+
+
+INFO 
+500 × 726
--- public/blackjack.js
+++ public/blackjack.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+const startButton = document.getElementById("myab");
+startButton.addEventListener("click", buttonClicked);
 
 const deck = [
   'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS',
@@ -8,13 +10,21 @@ const deck = [
   'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD'
 ];
 
+const Dealer = [];
 const player1 = [];
 const player2 = [];
-const player3 = [];
+const Table = [];
 
 const cardImages = {};
-
 function loadImages() {
+  // Add card back image
+  const cardBack = new Image();
+  cardBack.src = `cards/card-back.png`;
+  cardBack.onload = () => {
+    cardImages['card-back'] = cardBack;
+    checkAllImagesLoaded();
+  };
+
   let loadedImagesCount = 0;
 
   for (const card of deck) {
@@ -22,13 +32,18 @@ function loadImages() {
     img.src = `cards/${card}.png`;
     img.onload = () => {
       loadedImagesCount++;
-      if (loadedImagesCount === deck.length) {
-        init();
-      }
+      cardImages[card] = img;
+      checkAllImagesLoaded();
     };
-    cardImages[card] = img;
+  }
+
+  function checkAllImagesLoaded() {
+    if (loadedImagesCount === deck.length && cardImages['card-back']) {
+      init();
+    }
   }
 }
+
 
 function shuffle(array) {
   let currentIndex = array.length, temporaryValue, randomIndex;
@@ -47,36 +62,97 @@ function shuffle(array) {
 
 function dealCards() {
   for (let i = 0; i < 2; i++) {
+    Dealer.push(deck.pop());
     player1.push(deck.pop());
     player2.push(deck.pop());
-    player3.push(deck.pop());
   }
 }
 
 function drawCards() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#2ECC71'; // Set the fill color
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill the canvas with the color
   
-  // Draw player1's cards
+  // Draw dealer's cards
+  Dealer.forEach((card, index) => {
+    if (index === 0) {
+      ctx.drawImage(cardImages['card-back'], 600, 20, 125, 181);
+    } else {
+      ctx.drawImage(cardImages[card], 600 + index * 140, 20, 125, 181);
+    }
+  });
+  text("Dealer", 730, 230);
+
   player1.forEach((card, index) => {
-    ctx.drawImage(cardImages[card], 20 + index * 100, 20, 125, 181);
+    drawRotatedImage(cardImages[card], 100 + index * 170 - (index*50), 220+(index*120), 150, 217.8, 45);
   });
+  text("Player 1", 180, 600);
 
-  // Draw player2's cards
   player2.forEach((card, index) => {
-    ctx.drawImage(cardImages[card], 20 + index * 200, 200, 125, 181);
+    drawRotatedImage(cardImages[card], 1100 + index * 170 - (index * 50), 340 - (index * 120), 150, 217.8, -45);
   });
+  text("Player 2", 1275, 600);
+  text("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------", 0, 587);
+  drawTableCards(); 
+}
+//500 × 726
 
-  // Draw player3's cards
-  player3.forEach((card, index) => {
-    ctx.drawImage(cardImages[card], 20 + index * 300, 380, 125, 181);
+
+function drawRotatedImage(image, x, y, width, height, angleInDegrees) {
+  // Convert angle from degrees to radians
+  const angleInRadians = angleInDegrees * (Math.PI / 180);
+
+  // Save the current context state
+  ctx.save();
+
+  // Translate the context to the center of the image
+  ctx.translate(x + width / 2, y + height / 2);
+
+  // Rotate the context by the specified angle (in radians)
+  ctx.rotate(angleInRadians);
+
+  // Draw the image with the center of the image at the origin
+  ctx.drawImage(image, -width / 2, -height / 2, width, height);
+
+  // Restore the context to its original state
+  ctx.restore();
+}
+
+
+
+function text(text, width, height) {
+  ctx.font = "30px Arial"; // Change font style and size
+  ctx.fillStyle = "white"; // Change font color
+  ctx.textAlign = "center";
+  ctx.fillText(text, width, height);
+}
+
+function buttonClicked() {
+  Table.push(deck.pop());
+  Table.push(deck.pop());
+  drawTableCards();
+}
+
+
+function drawTableCards() {
+  Table.forEach((card, index) => {
+    ctx.drawImage(cardImages[card], 573 + index * 164, 420, 150, 217.8);
   });
 }
 
 
 function init() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   shuffle(deck);
   dealCards();
-  drawCards();
+  animate();
 }
+
+
+function animate() {
+  drawCards();
+  requestAnimationFrame(animate);
+}
+
 
 loadImages();
